34386afb54f62521a2d44c22d27513af3bfd43e0
emredemirel39 <emredemirel39@gmail.com>
2022-07-01 16:06:57 +0300

    Added app logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type { NextPage } from 'next'
 
 import styles from '../styles/Home.module.css'
@@ -13,73 +13,155 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputAdornment from '@mui/material/InputAdornment';
+import { IPaymentForm } from '../types';
 
 
 
 const Home: NextPage = () => {
 
+  const initialState: IPaymentForm = {
+    cardNumber: '',
+    cvv: '',
+    amount: '',
+    year: '',
+    month: ''
+  }
+
+  const now = new Date;
+  const currentYear = now.getFullYear();
+
+  const allNumsBetweenTwoNums = (num1: number, num2: number): number [] => {
+
+    const arr = [];
+    for (let i = num1; i <= num2; i++) {
+      arr.push(i)
+    }
+    return arr;
+  };
+
+  // dynamically updating year options of expiration date of credit card
+  const exptYearVariants = allNumsBetweenTwoNums(currentYear, (currentYear + 8));
+  const allMonths = allNumsBetweenTwoNums(1, 12);
+  const allMonthsFormatted = allMonths.map(month => ('0' + month).slice(-2));
+
+  const [ form, setForm ] = useState<IPaymentForm>(initialState);
+  const [ isValid, setIsValid ] = useState<boolean> (false);
+
+  const handleValidation = (): void => {
+    
+    const currentMonth = now.getMonth() + 1;
+    const cardNumberRegex = /^[0-9]{16}$/;
+    const cvvRegex =  /^[0-9]{3}$/;
+    const amountRegex =  /^[0-9]/;
+
+    switch (true) {
+
+      // if year of expiration day is current year, we cannot select months before current month
+      case Number(form.year) === currentYear && Number(form.month) < currentMonth :
+        setIsValid(false)
+        break;
+
+        case form.month.length === 0 || form.year.length === 0 :
+          setIsValid(false);
+          break;
+      
+        // if cardNumber input not matched cardnumber regex, so if it is null
+      case form.cardNumber.match(cardNumberRegex) === null :
+        setIsValid(false)
+        break;
+        
+      // if cvv input not matched cvv regex, so if it is null
+      case form.cvv.match(cvvRegex) === null :
+        setIsValid(false)
+        break; 
+
+      // if amount input not matched amount regex, so if it is null
+      case form.amount.match(amountRegex) === null :
+        setIsValid(false)
+        break; 
+    
+      default:
+        setIsValid(true)
+        break;
+    };
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    setForm({...form, [e.target.name]: e.target.value});
+  }
+
+  const testvali = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(isValid)
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    fetch('http://localhost:3000/api/payments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        CardNumber: form.cardNumber,
+        Cvv: form.cvv,
+        Amount: form.amount,
+        ExptDate: `${form.month}/${form.year}`
+      })
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    handleValidation();
+    // checking validation every time when form state is updating
+  }, [form])
 
   return (
     <div className={styles.homeWrapper} >
       <CssBaseline />
       <Container className={styles.container} maxWidth="lg">
 
-          <form className={styles.form}>
-            <TextField inputProps={{ maxLength: 16 }} fullWidth className={styles.cardNumberInput} id="outlined-basic" label="Card Number" size='small' variant="outlined" />
+          <form onSubmit={e => handleSubmit(e)} className={styles.form}>
+            <TextField onChange={(e) => handleChange(e)} name='cardNumber' inputProps={{ maxLength: 16 }} fullWidth className={styles.cardNumberInput} id="outlined-basic" label="Card Number" size='small' variant="outlined" />
             <div className={styles.dateAndCvvBox}>
 
-              <FormControl  sx={{ m: 1, minWidth: 120 }} size="small">
-                <InputLabel id="demo-select-small">Month</InputLabel>
-                <Select
-                  labelId="demo-select-small"
-                  id="demo-select-small"
-                  
-                  label="Age"
-                  onChange={() => console.log(12)}
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
+              <label className={styles.label} >Month</label>
+              <select onChange={(e) => handleChange(e)} className={styles.select} defaultValue='--' name="month">
+                <option disabled>--</option>
+                {
+                  allMonthsFormatted.map((month, i) => <option className={styles.option} key={i} value={month} >{month}</option>)
+                }
+              </select>
 
-              <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                <InputLabel id="demo-select-small">Year</InputLabel>
-                <Select
-                  labelId="demo-select-small"
-                  id="demo-select-small"
-                  
-                  label="Age"
-                  onChange={() => console.log(12)}
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
+              <label className={styles.label} >Year</label>
+              <select onChange={(e) => handleChange(e)} className={styles.select} defaultValue='--' name="year">
+                <option disabled>--</option>
+                {
+                  exptYearVariants.map((year, i) => <option className={styles.option} key={i} value={year} >{year}</option>)
+                }
+              </select>
 
-              <TextField inputProps={{ maxLength: 3 }} className={styles.cvvInput} id="outlined-basic" label="Cvv" size='small' variant="outlined" />
 
             </div>
+              <TextField onChange={(e) => handleChange(e)} name='cvv' inputProps={{ maxLength: 3 }} className={styles.cvvInput} id="outlined-basic" label="Cvv" size='small' variant="outlined" />
 
-            <FormControl fullWidth size='small' >
-          <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+            <FormControl  fullWidth size='small' >
+          <InputLabel className='amount' htmlFor="outlined-adornment-amount">Amount</InputLabel>
           <OutlinedInput
             id="outlined-adornment-amount"
-            
             startAdornment={<InputAdornment position="start">$</InputAdornment>}
             label="Amount"
+            name='amount'
+            onChange={(e) => handleChange(e)}
           />
         </FormControl>
 
           <div className={styles.buttonBox}>
-            <button disabled={false} >Submit</button>
+            <button disabled={isValid ? false : true } >Submit</button>
           </div>
 
           </form>
@@ -89,10 +171,20 @@ const Home: NextPage = () => {
               <div className={styles.cardChipWrapper}>
                 <div className={styles.cardChip} ></div>
               </div>
-              <input type="text" className={styles.creditCardNumber} />
+              <p className={styles.creditCardNumber} >
+                {
+
+                  form.cardNumber ? form.cardNumber : '****************'
+                  
+                }
+              </p>
               <div className={styles.cardBottomWrapper}>
                 <p className={styles.cardFullName} >Name Surname</p>
-                <input type="text" className={styles.cardExptDate} />              
+                <p className={styles.cardExptDate}>
+                    {
+                      `${form.month ? form.month : '--'} / ${form.year ? form.year : '----'}`
+                    }
+                </p>              
               </div>
             </div>
             
---
+++ types/index.ts
@@ -0,0 +1,7 @@
+export interface IPaymentForm {
+    cardNumber: string,
+    cvv: string,
+    amount: '',
+    year: '',
+    month: ''
+}
